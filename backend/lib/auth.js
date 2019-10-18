0ca06ef3adc9d8453a5a279b363f5926602d319e
emarkk <ema9628@gmail.com>
2019-10-18 09:33:04 +0200

    Add password protection for counters

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -1,7 +1,7 @@
 function login(credentials) {
     if(credentials.role === 'customer')
         return credentials;
-    if(credentials.role === 'counter' && [1,2,3].includes(credentials.id))
+    if(credentials.role === 'counter' && [1,2,3].includes(credentials.id) && credentials.password === ('counter' + credentials.id))
         return credentials;
     return null;
 }
