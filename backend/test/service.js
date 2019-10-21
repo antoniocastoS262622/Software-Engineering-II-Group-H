181035640bd0aaf2dfca761cc741e2cb8af039be
emarkk <ema9628@gmail.com>
2019-10-21 14:45:48 +0200

    Update tests

--- backend/test/service.js
+++ backend/test/service.js
@@ -28,11 +28,45 @@ suite('service.counter', () => {
     test('serveNext', async () => {
         const emit1 = Sinon.stub();
         const emit2 = Sinon.stub();
+        const emit3 = Sinon.stub();
+        const emit4 = Sinon.stub();
+        const emit5 = Sinon.stub();
+        const emit6 = Sinon.stub();
+        const emit7 = Sinon.stub();
         const db = {
             llen: Sinon.stub(),
             lpop: Sinon.stub(),
             get: Sinon.stub(),
-            hset: Sinon.stub()
+            hset: Sinon.stub(),
+            lrange: Sinon.stub()
+        };
+        const all = {
+            sockets: {
+                'abc': {
+                    auth: { role: 'customer' },
+                    ticket: { id: 6, requestType: 'accounts' },
+                    emit: emit3
+                },
+                'def': {
+                    auth: { role: 'counter', id: 2 },
+                    emit: emit4
+                },
+                'ghi': {
+                    auth: { role: 'customer' },
+                    ticket: { id: 26, requestType: 'packages' },
+                    emit: emit5
+                },
+                'jkl': {
+                    auth: { role: 'board' },
+                    emit: emit6
+                },
+                'mno': {
+                    auth: { role: 'customer' },
+                    ticket: { id: 13, requestType: 'accounts' },
+                    emit: emit7
+                },
+            },
+            emit: emit2
         };
 
         db.llen.onCall(0).resolves(0);
@@ -40,6 +74,10 @@ suite('service.counter', () => {
         db.llen.onCall(2).resolves(3);
         db.llen.onCall(3).resolves(2);
         db.llen.onCall(4).resolves(2);
+        
+        db.lrange.onFirstCall().resolves(['19', '22', '26', '27', '30']);
+        db.lrange.onSecondCall().resolves(['4', '6', '7', '11', '13', '16']);
+        db.lrange.onThirdCall().resolves(['4', '6', '7', '11', '13', '16']);
 
         db.lpop.onFirstCall().resolves(19);
         db.lpop.onSecondCall().resolves(4);
@@ -60,7 +98,6 @@ suite('service.counter', () => {
         await counter.serveNext(null, client2, db, null);
 
         const client3 = { auth: { role: 'counter', id: 3 }, emit: emit1 };
-        const all = { emit: emit2 };
         await counter.serveNext(null, client3, db, all);
         await counter.serveNext(null, client3, db, all);
 
@@ -76,6 +113,10 @@ suite('service.counter', () => {
         expect(db.get.calledWithExactly('tickets:19')).to.be.true();
         expect(db.get.calledWithExactly('tickets:4')).to.be.true();
 
+        expect(db.lrange.callCount).to.equal(3);
+        expect(db.lrange.calledWithExactly('queues:packages', '0', '-1')).to.be.true();
+        expect(db.lrange.calledWithExactly('queues:accounts', '0', '-1')).to.be.true();
+
         expect(emit1.callCount).to.equal(2);
         expect(emit1.calledWithMatch('nextClient', { counter: 3, code: 'P006' })).to.be.true();
         expect(emit1.calledWithMatch('nextClient', { counter: 3, code: 'A002' })).to.be.true();
@@ -83,6 +124,19 @@ suite('service.counter', () => {
         expect(emit2.callCount).to.equal(2);
         expect(emit2.calledWithMatch('serving', { counter: 3, code: 'P006' })).to.be.true();
         expect(emit2.calledWithMatch('serving', { counter: 3, code: 'A002' })).to.be.true();
+
+        expect(emit3.callCount).to.equal(1);
+        expect(emit3.calledWithExactly('estimatedTimeChanged', { estimatedTime: 10 })).to.be.true();
+
+        expect(emit4.callCount).to.equal(0);
+
+        expect(emit5.callCount).to.equal(1);
+        expect(emit5.calledWithExactly('estimatedTimeChanged', { estimatedTime: 27 })).to.be.true();
+
+        expect(emit6.callCount).to.equal(0);
+
+        expect(emit7.callCount).to.equal(1);
+        expect(emit7.calledWithExactly('estimatedTimeChanged', { estimatedTime: 25 })).to.be.true();
 
         expect(db.hset.callCount).to.equal(2);
         expect(db.hset.calledWithExactly('serving', 3, 'P006')).to.be.true();
@@ -98,10 +152,12 @@ suite('service.customer', () => {
         const db = {
             incr: Sinon.stub(),
             set: Sinon.stub(),
-            rpush: Sinon.stub()
+            rpush: Sinon.stub(),
+            lrange: Sinon.stub()
         };
-        db.incr.onFirstCall().resolves(5);
-        db.incr.onSecondCall().resolves(2);
+        db.incr.onFirstCall().resolves(12);
+        db.incr.onSecondCall().resolves(6);
+        db.lrange.onFirstCall().resolves(['9', '10', '12']);
 
         const client1 = { auth: { role: 'counter', id: 2 }, emit };
         await customer.getTicket(null, client1, null, null);
@@ -111,17 +167,20 @@ suite('service.customer', () => {
         await customer.getTicket({ requestType: 'accounts' }, client2, db, null);
 
         expect(emit.callCount).to.equal(1);
-        expect(emit.calledWithMatch('ticketGenerated', { code: 'A002' })).to.be.true();
+        expect(emit.calledWithMatch('ticketGenerated', { code: 'A006', estimatedTime: 15 })).to.be.true();
 
         expect(db.incr.callCount).to.equal(2);
         expect(db.incr.calledWithExactly('tickets:id')).to.be.true();
         expect(db.incr.calledWithExactly('queues:accounts:next')).to.be.true();
 
+        expect(db.lrange.callCount).to.equal(1);
+        expect(db.lrange.calledWithExactly('queues:accounts', '0', '-1')).to.be.true();
+
         expect(db.set.callCount).to.equal(1);
-        expect(db.set.calledWith('tickets:5')).to.be.true();
+        expect(db.set.calledWith('tickets:12')).to.be.true();
 
         expect(db.rpush.callCount).to.equal(1);
-        expect(db.rpush.calledWithExactly('queues:accounts', 5)).to.be.true();
+        expect(db.rpush.calledWithExactly('queues:accounts', 12)).to.be.true();
     });
 
 });
